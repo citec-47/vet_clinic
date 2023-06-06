693b0ee45815867d6aae786922eb79eff3356aab
citec-47 <mauricendonyi40@gmail.com>
2023-06-06 10:41:50 -0700

    BEGIN,ROLLBACK,COMMIT

--- queries.sql
+++ queries.sql
@@ -15,34 +15,33 @@ SELECT * FROM animals
 
 BEGIN;
 UPDATE animals SET species = 'unspecified';
+SELECT species from animals;
+ROLLBACK;
+SELECT species from animals;
 
+BEGIN;
 UPDATE animals SET species = 'digimon' WHERE name like '%mon';
 UPDATE animals SET species = 'pokemon' WHERE species NOT LIKE '%mon%';
-
+SELECT species from animals;
 COMMIT;
-
-SELECT * FROM animals WHERE species = 'digimon';
-SELECT * FROM animals
+SELECT species from animals;
 
 BEGIN;
-
 DELETE FROM animals;
-
+SELECT species from animals;
 ROLLBACK;
-
-DELETE FROM animals WHERE date_of_birth >= '2022-01-01';
+SELECT species from animals;
 
 BEGIN;
 
-UPDATE animals 
-SET weight_kg = weight_kg * -1
-WHERE weight_kg < 0;
+    DELETE FROM animals WHERE  date_of_birth > '2022/01/01' ;
+    SAVEPOINT SP1;
 
-SAVEPOINT SP1;
+    UPDATE animals SET weight_kg = weight_kg * -1; 
+    ROLLBACK TO SP1;
+    UPDATE animals SET weight_kg = weight_kg * -1 WHERE weight_kg < 0; 
 
-SELECT * FROM animals
-ROLLBACK TO SP1;
-COMMIT;
+    COMMIT;
 
 -- Answer for the quary
 
