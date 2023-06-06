417a55f63a247c7cc183db1d0e1a94223b5b0a0e
citec-47 <mauricendonyi40@gmail.com>
2023-06-06 09:22:02 -0700

    SELECT COUNT, ROLLBACK,BEGIN

--- README.md
+++ README.md
@@ -8,29 +8,6 @@ This repository includes files with plain SQL that can be used to recreate a dat
 
 <a name="readme-top"></a>
 
-<!--
-HOW TO USE:
-This is an example of how you may give instructions on setting up your project locally.
-
-Modify this file to match your project and remove sections that don't apply.
-
-REQUIRED SECTIONS:
-- Table of Contents
-- About the Project
-  - Built With
-  - Live Demo
-- Getting Started
-- Authors
-- Future Features
-- Contributing
-- Show your support
-- Acknowledgements
-- License
-
-After you're finished please remove all the comments and instructions!
--->
-
-
 <!-- TABLE OF CONTENTS -->
 
 # 📗 Table of Contents
@@ -100,13 +77,6 @@ To get a local copy up and running, follow these steps.
 
 In order to run this project you need:
 
-<!--
-Example command:
-
-```sh
- gem install rails
-```
- -->
 
 ### Setup
 
@@ -116,15 +86,6 @@ Clone this repository to your desired folder:
       https://github.com/citec-47/vet_clinic.git
 ```
 
-
-<!--
-Example commands:
-
-```sh
-  cd my-folder
-  git clone https://github.com/citec-47/vet_clinic.git
-```
---->
 
 ### Install
 
--- queries.sql
+++ queries.sql
@@ -10,29 +10,45 @@ SELECT * FROM animals WHERE name != 'Gabumon';
 SELECT * FROM animals WHERE weight_kg >= 10.3 AND weight_kg <= 17.3;
 
 /* Day 2. */
-BEGIN TRANSACTION;
+
+SELECT * FROM animals
+
+BEGIN;
 UPDATE animals SET species = 'unspecified';
-ROLLBACK;
 
 UPDATE animals SET species = 'digimon' WHERE name like '%mon';
-UPDATE animals SET species = 'pokemon' WHERE species IS NULL;
-COMMIT;
-END;
+UPDATE animals SET species = 'pokemon' WHERE species NOT LIKE '%mon%';
 
-BEGIN TRANSACTION;
+COMMIT;
+
+SELECT * FROM animals WHERE species = 'digimon';
+SELECT * FROM animals
+
+BEGIN;
+
 DELETE FROM animals;
-ROLLBACK;
-END;
 
-BEGIN TRANSACTION;
+ROLLBACK;
+
 DELETE FROM animals WHERE date_of_birth >= '2022-01-01';
-SAVEPOINT save_point;
-UPDATE animals SET weight_kg = weight_kg * -1;
-ROLLBACK TO SAVEPOINT save_point;
-UPDATE animals SET weight_kg = weight_kg * -1 WHERE weight_kg < 0;
+
+BEGIN;
+
+UPDATE animals 
+SET weight_kg = weight_kg * -1
+WHERE weight_kg < 0;
+
+SAVEPOINT SP1;
+
+SELECT * FROM animals
+ROLLBACK TO SP1;
 COMMIT;
 
+-- Answer for the quary
+
+SELECT * FROM animals
 SELECT COUNT(*) FROM animals;
+SELECT COUNT(escape_attempts) FROM animals WHERE escape_attempts = 0;
 SELECT COUNT(animals) FROM animals WHERE escape_attempts <= 0;
 SELECT AVG(weight_kg) FROM animals;
 SELECT MAX(escape_attempts), neutered FROM animals GROUP BY neutered;
